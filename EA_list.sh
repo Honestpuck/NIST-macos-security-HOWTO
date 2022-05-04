3fb1c429ba4aee6fbd2e97aebb6e2c41b6c9ffbe
Tony Williams <honestpuck@gmail.com>
2022-05-04 14:48:50 +1000

    More fiddling

--- EA_list.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-
-# cis v2 - Audit List
-
-echo "<result>"
-/usr/libexec/PlistBuddy -c "Print" /Library/Preferences/org.cis_lvl2_suncorp.audit.plist \
-  | grep -B 1 "finding = true"
-echo "</result>"
